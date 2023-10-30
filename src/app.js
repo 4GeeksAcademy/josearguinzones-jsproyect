51240c93fb6fe5d9798df95b319ce64ab901db31
JoseArguinzones <145572262+JoseArguinzones@users.noreply.github.com>
2023-10-30 23:21:02 +0000

    actualizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload =  function imprimir() {
+//   //write your code here
+  function imprimir() {
+  
+  
+    let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+     
+       const random1 = Math.floor(Math.random() * who.length);
+       const random2= Math.floor(Math.random() * action.length);
+       const random3= Math.floor(Math.random() * what.length);
+       const random4= Math.floor(Math.random() * when.length);
+   
+   
+   console.log(who[random1]+" "+action[random2]+" "+what[random3]+" "+when[random4]);
+   
+   }
+   let resultado=String;
+   resultado=imprimir();
+
+
+
+
